415640dc4fa56baa4eea83efd22b3c5b973af2f0
Lloyd <lloydollerhead.123@gmail.com>
2018-02-24 17:50:34 +0000

    add reversing a list example

---
+++ list-reverse.py
@@ -0,0 +1,20 @@
+def reverse_loop(my_list):
+    length = len(my_list)
+    for i in range(length // 2):
+        temp_value = my_list[i]
+        my_list[i] = my_list[(length - 1) - i]
+        my_list[(length - 1) - i] = temp_value
+    return my_list
+
+def reverse_recurse(my_list):
+    length = len(my_list)
+    if length <= 1:
+        return my_list
+    else:
+        reversed_list = reverse_recurse(my_list[1:])
+        reversed_list.append(my_list[0])
+        return reversed_list
+
+lloyd_list = [7, 5, 7, 3, 2, 4, 1, 2, 6]
+print(reverse_loop(lloyd_list))
+print(reverse_recurse(lloyd_list))
